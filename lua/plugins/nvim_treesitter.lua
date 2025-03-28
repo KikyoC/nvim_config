c0ef61bdb9640ab4454fd65ef8c23853864b5ee3
togauthi <togauthier23@gmail.com>
2025-03-28 09:41:33 +0100

    Update

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,13 +1,13 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
+    --[[ 'nvim-treesitter/nvim-treesitter', ]]
 
-    build = ':TSUpdate',
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        'nvim-treesitter/nvim-treesitter-refactor',
-        'windwp/nvim-ts-autotag',
-        'nvim-treesitter/playground',
-    },
+    --[[ build = ':TSUpdate', ]]
+    --[[ event = { 'BufReadPost', 'BufNewFile' }, ]]
+    --[[ dependencies = { ]]
+        --[[ 'nvim-treesitter/nvim-treesitter-textobjects', ]]
+        --[[ 'JoosepAlviste/nvim-ts-context-commentstring', ]]
+        --[[ 'nvim-treesitter/nvim-treesitter-refactor', ]]
+        --[[ 'windwp/nvim-ts-autotag', ]]
+        --[[ 'nvim-treesitter/playground', ]]
+    --[[ }, ]]
 }
--- lua/plugins/nvim_treesitter_context.lua
+++ lua/plugins/nvim_treesitter_context.lua
@@ -1,17 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "VeryLazy",
-    config = function()
-        require("treesitter-context").setup({
-        enable = true,
-        max_lines = 0,
-        min_window_height = 0,
-        line_numbers = true,
-        multiline_threshold = 20,
-        trim_scope = "outer",
-        mode = "cursor",
-        zindex = 20,
-        on_attach = nil,
-        })
-    end,
+	{
+		'nvim-treesitter/nvim-treesitter',
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				ensure_installed = { "c", "lua", "python" ,"css", "cpp", "javascript", "html" },
+				auto_install = true,
+				highlights = {
+					enable = true,
+				},
+				indent = { enable = true },
+			}
+		end,
+	}
 }
