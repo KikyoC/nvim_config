183a9297739192028a417f51fda4d54312054594
togauthi <togauthier23@gmail.com>
2025-04-09 13:25:17 +0200

    Crtl + f now open telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    }
+
+	vim.api.nvim_set_keymap('n', '<C-f>', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+
+}
