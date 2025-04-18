c2351e2186d4d953d9f02c5754a872cb8015c274
togauthi <togauthier23@gmail.com>
2025-04-18 17:42:14 +0200

    Better comments

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,25 +1,13 @@
 return {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
+    'numToStr/Comment.nvim',
     config = function()
-        require("Comment").setup({
-        pre_hook = function(ctx)
-            local U = require("Comment.utils")
-    
-            local location = nil
-            if ctx.ctype == U.ctype.block then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
-            end
-    
-            return require("ts_context_commentstring.internal").calculate_commentstring({
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-            location = location,
-            })
-        end,
-        })
-    end,
-    vim.api.nvim_set_keymap('n', '<C-/>', 'gcc', { noremap = false }),
-    vim.api.nvim_set_keymap('v', '<C-/>', 'gc', { noremap = false }),
+        -- Initialisation du plugin
+        require('Comment').setup()
+
+        -- Remapping pour commenter une ligne en mode normal
+        vim.keymap.set('n', '<C-/>', '<CMD>lua require("Comment.api").toggle.linewise.current()<CR>', { desc = 'Toggle comment for current line' })
+
+        -- Remapping pour commenter une sélection en mode visuel
+        vim.keymap.set('v', '<C-/>', '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', { desc = 'Toggle comment for selected block' })
+    end
 }
