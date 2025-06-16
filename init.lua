6099ff2851bd321039676903a9fa6b4a3fdb4793
togauthi <togauthier23@gmail.com>
2025-06-16 11:40:51 +0200

    coucou

--- init.lua
+++ init.lua
@@ -8,3 +8,8 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.number = true
+
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Q', 'q!', {})
